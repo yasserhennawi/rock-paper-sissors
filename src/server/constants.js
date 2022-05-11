da23ee5adadac759af58285688f79c8b1231253d
Yasser Hennawi <yahennawi@gmail.com>
2022-05-11 16:21:00 +0200

    add answers constants

--- src/server/constants.js
+++ src/server/constants.js
@@ -1,4 +1,20 @@
-const INPUTS = ["paper", "rock", "sissors"];
+// all "code" key could be array to allow multiple possible answers
+
+const INPUTS = [
+  { label: "Paper", code: "p" },
+  { label: "Rock", code: "r" },
+  { label: "Sissors", code: "s" },
+];
+
+const MC_ANSWERS = [
+  { label: "Yes", code: "y" },
+  { label: "No", code: "n" },
+];
+
+const GAME_MODE = [
+  { label: "Player VS Computer", code: "p" },
+  { label: "Computer VS Computer", code: "c" },
+];
 
 module.exports = {
   INPUTS,
